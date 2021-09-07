8a035b13fa7d16980a076d9ab302682e9b6d80f0
George Tudor <Alexander.George.Tudor@gmail.com>
2021-09-07 12:05:55 -0500

    refactored 1 b increment by 2 into a for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,7 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 5; i <=15; i++) {
+        for(int i = 0; i <=100; i = i + 2) {
             System.out.println(i);
         }
     }
