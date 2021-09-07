f6f04ea19faa8b17d7481d6f8419f724dcbbacbe
George Tudor <Alexander.George.Tudor@gmail.com>
2021-09-07 12:50:41 -0500

    Exercise 2, Fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,8 +2,16 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 100; i >= -10; i = i - 5) {
-            System.out.println(i);
+        for(int i = 1; i <= 100; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if(i % 3 == 0){
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
