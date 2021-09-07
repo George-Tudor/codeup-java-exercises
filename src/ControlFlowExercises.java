7ebdb79f8e0512eb353c29dcc03f17f3a70d4679
George Tudor <Alexander.George.Tudor@gmail.com>
2021-09-07 12:11:13 -0500

    refactored exercise 1b squaring by 2s to a for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,7 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 0; i <=100; i = i + 2) {
+        for(long i = 2; i < 1000000; i = i * i) {
             System.out.println(i);
         }
     }
