e77f4422bd9e10254b8470be583dcb2d7016237f
George Tudor <Alexander.George.Tudor@gmail.com>
2021-09-08 08:30:21 -0500

    complete Exercise #3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
 
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 1; i <= 100; i++) {
-            if(i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
-            } else if(i % 5 == 0) {
-                System.out.println("Buzz");
-            } else if(i % 3 == 0){
-                System.out.println("Fizz");
-            } else {
-                System.out.println(i);
+        boolean userChoice = true;
+        do {
+
+            Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer: ");
+        int userInput = scanner.nextInt();
+
+
+            System.out.println("number\t | squared\t | cubed");
+            System.out.println("------\t | ------\t | ------");
+            for (int i = 1; i <= userInput; i++) {
+                System.out.println(i + "\t\t |" + (i * i) + " \t \t |" + (i * i * i));
             }
-        }
+            System.out.println("Do you want to continue? (yes or no) ");
+            String yesOrNo = scanner.next();
+            if(!yesOrNo.equals("yes")) {
+                userChoice = false;
+            }
+        } while (userChoice);
     }
 }
