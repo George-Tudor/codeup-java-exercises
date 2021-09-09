d9b167ed9377a32d8c3c5d48bfe6f982a652a00c
George Tudor <Alexander.George.Tudor@gmail.com>
2021-09-09 14:09:30 -0500

    Methods exercise #2 complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
+
 public class MethodsExercises {
+    public static Scanner scanner = new Scanner(System.in);
 
     public static int addInts(int num1, int num2) {
         return num1 + num2;
@@ -20,11 +24,28 @@ public class MethodsExercises {
         return num1 % num2;
     }
 
-    public static void main(String[] args) {
-        System.out.println("The output for the addition method is " + addInts(27, 81));
-        System.out.println("The output for the subtraction method is " + subtractInts(9, 5));
-        System.out.println("The output for the multiplication method is " + multiplyInts(12, 16));
-        System.out.println("The output for the division method is " + divideInts(81, 9));
-        System.out.println("The output for the modulus method is " + modulo(81, 8));
+    public static int checkRange(int min, int max) {
+
+        int userInput = scanner.nextInt();
+        if (userInput >= min && userInput <= max) {
+            System.out.println("Your number is " + userInput);
+            return userInput;
+
+        }   System.out.println("The number must be between " + min + " and " + max + ", try again.");
+            return checkRange(min, max);
+
+        }
+
+
+        public static void main (String[] args){
+            System.out.println("The output for the addition method is " + addInts(27, 81));
+            System.out.println("The output for the subtraction method is " + subtractInts(9, 5));
+            System.out.println("The output for the multiplication method is " + multiplyInts(12, 16));
+            System.out.println("The output for the division method is " + divideInts(81, 9));
+            System.out.println("The output for the modulus method is " + modulo(81, 8));
+            System.out.println("Enter a number between 1 and 10: ");
+            int userInput = checkRange(1, 10);
+
+        }
     }
-}
+
