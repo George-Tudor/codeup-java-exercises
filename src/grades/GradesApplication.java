fb53c314a5659455c638258430c4047f9fac89fe
George Tudor <Alexander.George.Tudor@gmail.com>
2021-09-21 22:40:41 -0500

    completed #3 of Student Grades App

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class GradesApplication {
 
     public static void main(String[] args) {
-        Map<String, Student> userNames = new HashMap<>();
+        Map<String, Student> studentMap = new HashMap<>();
         Student kenny = new Student("Kenny McCormick");
         Student cartman = new Student("Eric Cartman");
         Student stan = new Student("Stan Marsh");
@@ -28,20 +28,32 @@ public class GradesApplication {
         kyle.addGrade(82);
         kyle.addGrade(88);
 
-        userNames.put("mysterion", kenny);
-        userNames.put("awesomeo3000", cartman);
-        userNames.put("toolshed", stan);
-        userNames.put("humankite", kyle);
+        studentMap.put("mysterion", kenny);
+        studentMap.put("awesomeo3000", cartman);
+        studentMap.put("toolshed", stan);
+        studentMap.put("humankite", kyle);
 
         System.out.println("Welcome!\n");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Here are the GitHub usernames of our students: \n");
-        List<String> userName = new ArrayList<>(userNames.keySet());
+        List<String> userName = new ArrayList<>(studentMap.keySet());
         System.out.println(userName + "\n");
-        System.out.println("Which student would you like to see more information on? \n");
-        String input = scanner.nextLine();
-        
+        String input;
+        do {
+            System.out.println("Which student would you like to see more information on? \n");
 
+            input = scanner.nextLine();
+            if (studentMap.containsKey(input)) {
+                Student s = studentMap.get(input);
+                System.out.println("\nName: " + s.getName() + " - GitHub Username: " + input);
+                System.out.println("Current Average: " + s.getGradeAverage(s.getGrades()));
+            } else {
+                System.out.println("Sorry, no student found with the Github username of " + input);
+            }
+            System.out.println("Would you like to see another student?");
+            input = scanner.nextLine();
+        } while (!"no".equals(input));
+        System.out.println("Goodbye, and have a wonderful day!");
 
     }
 }
