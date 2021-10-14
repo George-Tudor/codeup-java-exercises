65f7e6d505b38e08edaed2b448a39d0347600c94
George Tudor <Alexander.George.Tudor@gmail.com>
2021-10-14 09:32:56 -0500

    java_exercises added instructions and charArray methodC

--- src/JavaDrills.java
+++ src/JavaDrills.java
@@ -1,11 +1,19 @@
-import java.lang.StringBuilder.*;
-//WORK IN PROGRESS
+//================================= WARM UP
+//        In `codeup-java-exercises`, create a class `JavaDrills`, with the following:
+//        Create a public static method, `flipOuterCase`, that takes in a string and returns the string
+//        with the case reversed for the first and last letters. Assume the input will be three or more characters long.
+//        Example inputs and expected outputs...
+//        System.out.println(flipOuterCase(“cat”)); // CaT
+//        System.out.println(flipOuterCase(“CaT”)); // cat
+//        System.out.println(flipOuterCase(“caT”)); // Cat
+//        System.out.println(flipOuterCase(“cAt”)); // CAT
+
 public class JavaDrills {
     public static String flipOuterCase(String word) {
-       // String newString = '';
+        char[] charArray = word.toCharArray();
         for(int i = 0; i < word.length(); i++) {
-            if(Character.isLowerCase(word.charAt(i))) {
-                //newString.setCharAt() += word.charAt(i).
+            if(Character.isLowerCase(word.charAt(i)) {
+                //word.charAt(i).
             }
         }
     }
