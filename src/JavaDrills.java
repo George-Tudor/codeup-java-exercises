49aefdf8a4f13625137d034bbbfe4ccbfbc22c5a
George Tudor <Alexander.George.Tudor@gmail.com>
2021-10-14 09:26:26 -0500

    java_exercises first commit

---
+++ src/JavaDrills.java
@@ -0,0 +1,12 @@
+import java.lang.StringBuilder.*;
+//WORK IN PROGRESS
+public class JavaDrills {
+    public static String flipOuterCase(String word) {
+       // String newString = '';
+        for(int i = 0; i < word.length(); i++) {
+            if(Character.isLowerCase(word.charAt(i))) {
+                //newString.setCharAt() += word.charAt(i).
+            }
+        }
+    }
+}
